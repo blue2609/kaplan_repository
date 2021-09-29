8705dbd1d2b0f2642b134e42fe6c4e0eed63415b
Stanley Setiawan <stanley.setiawan@kaplan.edu.au>
2021-09-29 12:00:01 +1000

    1. Created 'Prod' Sub-Directory which will save all PowerShell Scripts used to deploy remote apps in Prod server" 2. Created ExploratoryDesktop.ps1 in the 'Prod' sub-directory 3. Created OrangeDataMining.ps1 in the 'Prod' sub-directory

---
+++ publishAppsScripts/Prod/OrangeDataMining.ps1
@@ -0,0 +1,8 @@
+New-RDRemoteApp -Collectionname "MBATS" `
+-Alias "Orange" `
+-DisplayName "Orange" `
+-filePath "C:\Program Files\Orange\pythonw.exe" `
+-CommandLineSetting Require `
+-IconPath "C:\Program Files\Orange\share\orange3\icons\Orange.ico" `
+-RequiredCommandLine "-m Orange.canvas" `
+-ConnectionBroker kap-aws-rdsb-1.kaplan-student.edu.au 
