4695580327cadc46c8ab305df1c2a96112d0d00b
Stanley Setiawan <stanley.setiawan@kaplan.edu.au>
2021-09-29 11:28:22 +1000

    Renamed publishExploratory.ps1 to ExploratoryDesktop.ps1 and moved it to the 'publishAppScripts' directory

---
+++ publishAppsScripts/ExploratoryDesktop.ps1
@@ -0,0 +1,7 @@
+New-RDRemoteApp `
+-Collectionname "MBATS" `
+-Alias "Exploratory" `
+-DisplayName "Exploratory" `
+-filePath "c:\windows\system32\WindowsPowershell\v1.0\powershell.exe" `
+-iconPath "C:\Program Files\Exploratory\logo.ico" `
+-ConnectionBroker kap-aws-rdsb-1n.non-prod.kaplan-student.edu.au
