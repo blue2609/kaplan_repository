70ce6f4e21f277e66ca99017e1d6274fddca6917
Stanley Setiawan <stanley.setiawan@kaplan.edu.au>
2021-11-09 15:41:52 +1100

    - 'install_mamba.ps1' can now take in the switch parameter 'GetLatestMamba' which will dicate whether or not system administrator wants to download the latest version of 'mamba_installer.exe' before creating the conda/mamba virtual environments - The script 'install_mamba.ps1' will add the relevant conda and mamba paths to system path on Windows. This will make conda and mamba accessible from PowerShell/Command Prompt - The script 'install_mamba.ps1' will configure 'pip_interop_enabled' to True so that conda/mamba can work better with pip installed packages - Finally, the script 'install_mamba.ps1' will initialise conda by executing 'conda init'

--- RebuildApplication/install_mamba.ps1
+++ RebuildApplication/install_mamba.ps1
@@ -1,4 +1,35 @@
+[CmdletBinding()]
+param (
+	[switch]$GetLatestMamba
+)
+
+if ($GetLatestMamba) {
+	Invoke-WebRequest `
+	-Uri "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Windows-x86_64.exe" `
+	-OutFile "mamba_installer.exe"
+}
+
 Start-Process `
--FilePath "mamba_installer.exe"
-# -ArgumentList "APPDIR='C:\ProgramData\miniforge' /quiet" `
-# -Wait
+-FilePath "./mamba_installer.exe" `
+-ArgumentList "/S" `
+-Wait
+
+# Add relevant mamba and conda paths to $env:Path environment variable to make
+# conda and mamba accessible from PowerShell and Command Prompt
+$mamba_root_dir = "C:\ProgramData\mambaforge"
+$mamba_paths = @(
+	($mamba_root_dir),
+	($mamba_root_dir + "\condabin"),
+	($mamba_root_dir + "\Library\bin"),
+	($mamba_root_dir + "\Library\usr\bin"),
+	($mamba_root_dir + "\Library\mingw-w64\bin"),
+	($mamba_root_dir + "\Scripts")
+)
+$env:Path += $mamba_paths -join ";"
+[System.Environment]::SetEnvironmentVariable('Path',$env:Path,'Machine')
+
+# change conda config so virtual env can work together better with pip 
+conda config --set pip_interop_enabled True
+
+# initialise conda
+conda init
