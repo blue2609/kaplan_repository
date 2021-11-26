c6831a8b6cd00a7f49d736c79b83ed29a13c2b9d
Stanley Setiawan <stanley.setiawan@kaplan.edu.au>
2021-11-26 18:02:09 +1100

    - Added 'publish_apps_config.json' that specifies configuration for the RDS Collection Name and Broker. 'publish_apps.ps1' script now reads this JSON config file - The 'IconPath' parameter for 'New-RDRemoteApp' cmdlet inside 'publish_apps.ps1' is using the full path of the icon file (.ico) from now on. This is done to prevent errors when publishing remote apps

--- RebuildApplication/Rebuild_Conda_Env/publish_apps.ps1
+++ RebuildApplication/Rebuild_Conda_Env/publish_apps.ps1
@@ -4,17 +4,23 @@ Param(
 	[string]$RunCmdDir
 )
 
+$publish_app_config = Get-Content -Raw -Path ./publish_apps_config.json | ConvertFrom-Json
+$CollectionName = $publish_app_config.CollectionName
+$ConnectionBroker = $publish_app_config.ConnectionBroker
+
 $publish_apps_json = Get-ChildItem "./publish_apps" | Where-Object {$_.Name -like '*.json'}
 foreach ($json_file in $publish_apps_json) {
 	$venv = Get-Content -Raw -Path $json_file.FullName | ConvertFrom-Json
 	foreach ($app in $venv) {
+		$full_icon_path = (Join-Path $PWD $app.icon_path) | Resolve-Path
 		New-RDRemoteApp `
-		-Collectionname "MBATS" `
+		-Collectionname $CollectionName `
 		-Alias $app.alias `
 		-DisplayName $app.display_name `
 		-FilePath "c:\windows\system32\WindowsPowershell\v1.0\powershell.exe" `
+		-CommandLineSetting Require `
 		-RequiredCommandLine $RunCmdDir"\"$($app.display_name)".ps1" `
-		-IconPath $app.icon_path `
-		-ConnectionBroker kap-aws-rdsb-1.kaplan-student.edu.au
+		-IconPath $full_icon_path `
+		-ConnectionBroker $ConnectionBroker
 	}
 }
